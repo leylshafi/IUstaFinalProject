0c494bd68af22ca91d571a5dc0bae4df3e792eaa
Leyla Shafiyeva <llshafi03@gmail.com>
2023-10-01 18:35:34 +0400

    Add logging

--- Presentation/IUstaFinalProject.Api/Program.cs
+++ Presentation/IUstaFinalProject.Api/Program.cs
@@ -7,62 +7,66 @@ using IUstaFinalProject.Persistence;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Serilog;
+using Serilog.Core;
 using System.Text;
 
-internal class Program
-{
-    private static void Main(string[] args)
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices();
+builder.Services.AddPersistenceService();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+ policy.WithOrigins("https://localhost:7176", "http://localhost:7176").AllowAnyHeader().AllowAnyMethod()
+));
+
+Logger log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.MSSqlServer(builder.Configuration.GetConnectionString("Default"),"logs")
+    .CreateLogger();
+
+builder.Host.UseSerilog(log);
+
+builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
+    .AddFluentValidation(config => config.RegisterValidatorsFromAssemblyContaining<AgreementDtoValidator>())
+    .ConfigureApiBehaviorOptions(o => o.SuppressModelStateInvalidFilter = true);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Admin", options =>
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddApplicationServices();
-        builder.Services.AddInfrastructureServices();
-        builder.Services.AddPersistenceService();
-        
-        builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-         policy.WithOrigins("https://localhost:7176", "http://localhost:7176").AllowAnyHeader().AllowAnyMethod()
-        ));
-
-
-        builder.Services.AddControllers(options => options.Filters.Add<ValidationFilter>())
-            .AddFluentValidation(config => config.RegisterValidatorsFromAssemblyContaining<AgreementDtoValidator>())
-            .ConfigureApiBehaviorOptions(o => o.SuppressModelStateInvalidFilter = true);
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer("Admin",options =>
-            {
-                options.TokenValidationParameters = new()
-                {
-                    ValidateAudience = true,
-                    ValidateIssuer = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-
-                    ValidAudience = builder.Configuration["JWT:Audience"],
-                    ValidIssuer = builder.Configuration["JWT:Issuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecurityKey"])),
-                    LifetimeValidator = (notBefore,expires,securityToken,validationParameters) => expires!=null? expires>DateTime.UtcNow:false
-                };
-            });
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
+        options.TokenValidationParameters = new()
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-        app.UseCors();
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
 
-        app.UseHttpsRedirection();
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecurityKey"])),
+            LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires != null ? expires > DateTime.UtcNow : false
+        };
+    });
+var app = builder.Build();
 
-        app.UseAuthentication();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
