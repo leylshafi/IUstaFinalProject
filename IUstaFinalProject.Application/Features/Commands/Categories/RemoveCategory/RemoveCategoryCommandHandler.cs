2f2d94547d7f9c15c46beba354f195a14a832d55
Leyla Shafiyeva <llshafi03@gmail.com>
2023-10-02 00:02:16 +0400

    Admin controller

---
+++ IUstaFinalProject.Application/Features/Commands/Categories/RemoveCategory/RemoveCategoryCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using IUstaFinalProject.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IUstaFinalProject.Application.Features.Commands.Categories.RemoveCategory
+{
+    public class RemoveCategoryCommandHandler:IRequestHandler<RemoveCategoryCommandRequest, RemoveCategoryCommandResponse>
+    {
+        private readonly ICategoryWriteRepository repository;
+
+        public RemoveCategoryCommandHandler(ICategoryWriteRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<RemoveCategoryCommandResponse> Handle(RemoveCategoryCommandRequest request, CancellationToken cancellationToken)
+        {
+            await repository.RemoveAsync(request.CategoryId.ToString());
+            return new();
+        }
+    }
+}
--- Presentation/IUstaFinalProject.Api/Controllers/AdminController.cs
+++ Presentation/IUstaFinalProject.Api/Controllers/AdminController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IUstaFinalProject.Domain.Entities.Dtos;
+using IUstaFinalProject.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using IUstaFinalProject.Application.Features.Commands.Categories.AddCategory;
+using MediatR;
+using IUstaFinalProject.Application.Repositories;
+using IUstaFinalProject.Application.Features.Commands.Categories.RemoveCategory;
 
 namespace IUstaFinalProject.Api.Controllers
 {
@@ -9,5 +16,83 @@ namespace IUstaFinalProject.Api.Controllers
     [Authorize(AuthenticationSchemes ="Admin")]
     public class AdminController : ControllerBase
     {
+        private readonly ICategoryReadRepository productReadRepository;
+        private readonly ICategoryWriteRepository productWriteRepository;
+
+        private readonly IMediator mediator;
+        private readonly ILogger<AdminController> logger;
+
+        public AdminController(ICategoryReadRepository productReadRepository, ICategoryWriteRepository productWriteRepository, IMediator mediator, ILogger<AdminController> logger)
+        {
+            this.productReadRepository = productReadRepository;
+            this.productWriteRepository = productWriteRepository;
+            this.mediator = mediator;
+            this.logger = logger;
+        }
+
+
+        [HttpPost("add category")]
+        public async Task<IActionResult> Add([FromBody] AddCategoryCommandRequest request)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await mediator.Send(request);
+                    return StatusCode((int)HttpStatusCode.Created);
+                }
+                return BadRequest(ModelState);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        [HttpPost("remove")]
+        public async Task<IActionResult> Remove([FromBody] RemoveCategoryCommandRequest request)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await mediator.Send(request);
+                    return StatusCode((int)HttpStatusCode.Created);
+                }
+                return BadRequest(ModelState);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+
+        //[HttpGet("statistics")]
+        //public IActionResult GetStatistics()
+        //{
+        //    try
+        //    {
+        //        var customerCount = workerDbContext.Customers.Count();
+        //        var workerCount = workerDbContext.Workers.Count();
+        //        var categoryCount = workerDbContext.Categories.Count();
+
+        //        var statistics = new
+        //        {
+        //            CustomerCount = customerCount,
+        //            WorkerCount = workerCount,
+        //            CategoryCount = categoryCount
+        //        };
+
+        //        return Ok(statistics);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+        //    }
+        //}
     }
 }
